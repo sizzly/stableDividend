5c04e87f9ebda0b5790e6f8bf68475566fdcef87
sizzly <rob.tacey@yahoo.com>
2023-02-27 18:45:51 -0500

    Change to ticker data file

--- getData.py
+++ getData.py
@@ -2,9 +2,9 @@ import requests
 import json
 import time
 
-thislist = ["T.TO", "MFC.TO", "CU.TO", "SLF.TO", "IGM.TO", "BCE.TO", "ENB.TO", "ALA.TO", "LIF.TO", "GEI.TO"]
-for x in thislist:
-      time.sleep(15)
+List = open("tickerlist").readlines()
+for x in List:
+      #time.sleep(15)
       print(x)
       tickerstring = './data/'
       tickerstring += x
@@ -13,9 +13,3 @@ for x in thislist:
       url = 'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&apikey=BRYEQK2HK6SPAM62&symbol='
       url += x
       print(url)
-
-      r = requests.get(url)
-      data = r.json()
-
-      with open(tickerstring, 'w', encoding='utf-8') as f:
-          json.dump(data, f, ensure_ascii=False, indent=4)
