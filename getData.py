4285bd3ba76b27fe8da99fbc4b3da0e52fe8e47e
sizzly <rob.tacey@yahoo.com>
2023-02-27 18:27:48 -0500

    Update getData.py

--- getData.py
+++ getData.py
@@ -4,15 +4,16 @@ import time
 
 thislist = ["T.TO", "MFC.TO", "CU.TO", "SLF.TO", "IGM.TO", "BCE.TO", "ENB.TO", "ALA.TO", "LIF.TO", "GEI.TO"]
 for x in thislist:
-  print(x)
-  tickerstring = './data/'
-  tickerstring += x
-  tickerstring += '.data'
-  print(tickerstring)
-  url = 'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&apikey=BRYEQK2HK6SPAM62&symbol='
-  url += x
-  print(url)
-  time.sleep(2)
+      time.sleep(2)
+      print(x)
+      tickerstring = './data/'
+      tickerstring += x
+      tickerstring += '.data'
+      print(tickerstring)
+      url = 'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&apikey=BRYEQK2HK6SPAM62&symbol='
+      url += x
+      print(url)
+
   #r = requests.get(url)
   #data = r.json()
 
