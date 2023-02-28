1c576fd6d3338e2431fe0dc153e09f303e87c13f
sizzly <rob.tacey@yahoo.com>
2023-02-28 07:05:28 -0500

    Update crunchNumbers.py

--- crunchNumbers.py
+++ crunchNumbers.py
@@ -24,9 +24,7 @@ for x in List:
               if line_count == 0:
                   line_count += 1
               elif line_count < 53:
-                  n = []
-                  n.append(row[5])
-                  sum52weeks += n
+                  sum52weeks = float(sum52weeks) + float(row[5])
                   print(f'\t{row[5]}')
                   line_count += 1
 
