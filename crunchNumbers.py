7d8a07ffc4d90e787680218244bfdb636193e6b9
sizzly <rob.tacey@yahoo.com>
2023-02-28 07:20:27 -0500

    Update crunchNumbers.py

--- crunchNumbers.py
+++ crunchNumbers.py
@@ -16,7 +16,11 @@ for x in List:
       print(tickerstring)
 
       sum52weeks = 0
+      mean = 0
+      variance = 0
+      sumvariance = 0
 
+      # Get the Average Price
       with open(tickerstring) as csv_file:
           csv_reader = csv.reader(csv_file, delimiter=',')
           line_count = 0
@@ -25,9 +29,26 @@ for x in List:
                   line_count += 1
               elif line_count < 53:
                   sum52weeks = float(sum52weeks) + float(row[5])
-                  print(f'\t{row[5]}')
                   line_count += 1
 
           print(f'Processed {line_count} lines for a total of {sum52weeks}.')
+          mean = float(sum52weeks) / 52
+
+          # Get the deviation
+          line_count = 0
+          pav-pi = 0
+          pav-pi2 = 0
+          for row in csv_reader:
+              if line_count == 0:
+                  line_count += 1
+              elif line_count < 53:
+                  pav-pi = float(row[5]) - float(mean)
+                  pav-pi2 = float(pav-pi) * float(pav-pi)
+                  sumvariance = float(sumvariance) + float(pav-pi2)
+                  line_count += 1
+
+          variance = float(sumvariance) / 52
+          print(f'Variance is {variance}.')
+
 
 # Output the 20 stocks
