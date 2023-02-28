77f8e3d033bcd5d8ca7b80e33ceb49889916442a
sizzly <rob.tacey@yahoo.com>
2023-02-28 09:39:34 -0500

    Update getData.py

--- getData.py
+++ getData.py
@@ -9,6 +9,7 @@ for x in List:
       tickerstring = ./data/
       tickerstring += x
       tickerstring += .csv
+      tickerstring = str(tickerstring)
       #print(tickerstring)
       url = 'https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&apikey=BRYEQK2HK6SPAM62&datatype=csv&symbol='
       url += x
